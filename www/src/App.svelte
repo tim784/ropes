41486efbe98887a33ae05de344bba55886a7e4d3
tim784 <>
2024-08-16 05:05:14 -0500

    play with www

--- www/src/App.svelte
+++ www/src/App.svelte
@@ -4,16 +4,14 @@
 </script>
 
 <main>
-  <div>
+  <a href="/index.user.js" class="install-button">
+    <div class="install-button-text">Install</div>
     <img src={ropesLogo} class="logo" alt="Ropes Logo" />
-  </div>
+  </a>
+
   <h1>Ropes <span>for Empornium</span></h1>
 
-  <p>
-    Get the latest version <a href="/index.user.js">here</a>.
-  </p>
-
-  <p>
+  <p class="github">
     <a href={packageJson.repository.url}>Github</a>
   </p>
 </main>
@@ -25,7 +23,47 @@
     will-change: filter;
     transition: filter 300ms;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #cc9029aa);
+
+  .install-button-text {
+    font-size: 2em;
+    font-weight: bold;
+  }
+
+  .install-button {
+    color: #020617;
+    background-color: #f1f5f9;
+    border-radius: 1rem;
+    border-color: #eee;
+    border-width: 1px;
+    border-style: solid;
+    padding: 1rem;
+    display: block;
+    box-shadow:
+      0 10px 15px -3px rgb(0 0 0 / 0.1),
+      0 4px 6px -4px rgb(0 0 0 / 0.1);
+    aspect-ratio: 1/1;
+    max-width: fit-content;
+    margin-inline-start: auto;
+    margin-inline-end: auto;
+    transition: box-shadow 300ms;
+    transition: filter 300ms;
+    text-decoration: none;
+  }
+
+  .install-button:hover,
+  .install-button:focus-visible {
+    filter: drop-shadow(0 0 1em #cc902988);
+  }
+
+  .github {
+    font-size: 1.5em;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .install-button {
+      background-color: #1e293b;
+      color: #f8fafc;
+      border-color: #222;
+    }
   }
 </style>
