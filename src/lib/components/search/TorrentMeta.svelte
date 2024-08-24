c3b595c6ea753aef63855231f79dc65d508d728b
tim784 <176785046+tim784@users.noreply.github.com>
2024-08-24 15:40:37 -0500

    Torrent link should not be a flex child (but text inside should be)

--- src/lib/components/search/TorrentMeta.svelte
+++ src/lib/components/search/TorrentMeta.svelte
@@ -50,15 +50,17 @@
 <div class="flex flex-col">
   <TorrentBadges {torrent} {hasSeen} {isBookmarked} {isPersonalDoubleseed} {isPersonalFreeleech} />
 
-  <Link
-    href={torrent.pageHref}
-    class="my-2 underline hover:text-foreground/80"
-    variant="foreground"
-  >
-    <h3 class="inline text-xl font-bold tracking-tight">
-      {torrent.name}
-    </h3>
-  </Link>
+  <div class="my-2">
+    <Link
+      href={torrent.pageHref}
+      class="underline hover:text-foreground/80"
+      variant="foreground"
+    >
+      <h3 class="inline text-xl font-bold tracking-tight">
+        {torrent.name}
+      </h3>
+    </Link>
+  </div>
 
   <div class="space-y-0.5">
     <div>
