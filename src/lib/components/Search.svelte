163ff61601a28de73a41c5a3aa0643ecf2bc7053
tim784 <176785046+tim784@users.noreply.github.com>
2024-08-26 17:49:51 -0500

    Improve pagination data

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -6,26 +6,28 @@
   import ThemeTest from '$components/ThemeTest.svelte';
   import HeaderBox from './search/HeaderBox.svelte';
   import { formatNumber } from '../util';
+  import { defaultPagination } from '$gather/pagination';
 
   export let page: Page<SearchPageData>;
 
-  let resultCount = 0;
+  let curPagination = defaultPagination();
 
   $: {
     page.dataPromise.then((p) => {
-      resultCount = p.pagination.resultCount;
+      curPagination = p.pagination;
     });
   }
+  $: totalResultCount = formatNumber(curPagination.totalResultCount);
 </script>
 
 <div class="relative flex flex-col items-start overflow-visible bg-inherit md:flex-row">
   <aside
-    class="bg-background p-4 md:sticky md:left-0 md:top-[var(--header-height)] md:h-[calc(100vh_-_var(--header-height))] md:min-w-[var(--search-form-width)] md:max-w-[var(--search-form-width)] md:overflow-y-scroll md:border-r w-full"
+    class="w-full bg-background p-4 md:sticky md:left-0 md:top-[var(--header-height)] md:h-[calc(100vh_-_var(--header-height))] md:min-w-[var(--search-form-width)] md:max-w-[var(--search-form-width)] md:overflow-y-scroll md:border-r"
   >
     <Form />
   </aside>
 
-  <div class="w-full space-y-4 py-4 px-16">
+  <div class="w-full space-y-4 px-16 py-4">
     {#if import.meta.env.DEV}
       <ThemeTest />
     {/if}
@@ -34,7 +36,7 @@
 
     <div class="text-2xl">
       <h2 class="inline font-bold">Results</h2>
-      <span>({formatNumber(resultCount)})</span>
+      <span>({totalResultCount})</span>
     </div>
 
     <Paginator {page} />
--- src/lib/gather/pagination.ts
+++ src/lib/gather/pagination.ts
@@ -1,10 +1,53 @@
 import { querySelector, removeNonNumericChars } from './util';
 
-export type Pagination = {
-  currentPage: number;
-  totalPages: number;
-  resultCount: number;
+export type ResultRange = {
+  start: number;
+  end: number;
 };
+
+export type Pagination = {
+  /**
+   * The number of results on a full (normal) page
+   */
+  resultsPerPage: number;
+
+  /**
+   * The number of results on the current page, which may be less than the
+   * number of results on a full page if this is the last page.
+   */
+  resultsOnThisPage: number;
+
+  /**
+   * The range of results on the current page, as a tuple of [start, end]
+   */
+  thisPageRange: ResultRange;
+
+  /**
+   * The current page number
+   */
+  currentPage: number;
+
+  /**
+   * The total number of pages
+   */
+  totalPages: number;
+
+  /**
+   * The total number of results
+   */
+  totalResultCount: number;
+};
+
+export function defaultPagination(): Pagination {
+  return {
+    resultsPerPage: 0,
+    resultsOnThisPage: 0,
+    thisPageRange: { start: 0, end: 0 },
+    currentPage: 1,
+    totalPages: 1,
+    totalResultCount: 0
+  };
+}
 
 function getPageFromUrl(url: string): number {
   const urlObj = new URL(url, window.location.href);
@@ -21,31 +64,87 @@ function getResultCount(doc: Document) {
   return resultCount;
 }
 
-export function getPagination(doc: Document): Pagination {
-  // cases:
-  // 1. 0 or at most 1 page of results: .pager has no child elements, therefore
-  //    currentPage = 1, totalPages = 1
-  //
-  // 2. otherwise:
-  //
-  //    currentPage: can be got a couple of ways, but the simplest seems to be
-  //    looking at one of the torrent table's sort button URLs. it's a query
-  //    param named `page`, or 1 if it's not there
-  //
-  //    totalPages: ditto, but on the link of .pager_last. if there is no .pager_last,
-  //    then its the same as currentPage
-  const resultCount = getResultCount(doc);
+// matches 1-100, for example
+//
+// we match against commas too to be safe, even though the site doesn't appear
+// to use them.
+const resultsOnThisPagePattern = /(?<start>[0-9,]+)-(?<end>[0-9,]+)/;
 
-  const pagerElement = querySelector('.linkbox.pager', doc);
-  const hasChildNodes = pagerElement?.hasChildNodes();
-  if (!hasChildNodes) {
-    return {
+function getPageResultRange(pagerText: string): ResultRange {
+  const match = pagerText.match(resultsOnThisPagePattern);
+  if (!match) {
+    throw new Error(`Could not parse pager text: ${pagerText}`);
+  }
+  const start = parseInt(removeNonNumericChars(match.groups!.start));
+  const end = parseInt(removeNonNumericChars(match.groups!.end));
+  return { start, end };
+}
+
+function resultsInRange(resultRange: ResultRange): number {
+  return resultRange.end - resultRange.start + 1;
+}
+
+export function getPagination(doc: Document): Pagination {
+  /*
+  cases:
+
+  - 0 results: .linkbox.pager has no children
+  
+    {
+      resultsPerPage: 0,
+      resultsOnThisPage: 0,
+      totalResultCount: 0
       currentPage: 1,
       totalPages: 1,
-      resultCount
+    }
+
+   - 1 page of results: .linkbox.pager has no children, but resultCount > 0
+
+     {
+       resultsPerPage: resultCount,
+       resultsOnThisPage: resultCount,
+       totalResultCount: resultCount
+       currentPage: 1,
+       totalPages: 1,
+     }
+
+   - otherwise: .linkbox.pager exists, and has children
+
+     {
+       resultsPerPage: resultCount,
+       resultsOnThisPage: resultsOnThisPage,
+       totalResultCount: resultCount
+       currentPage: currentPage,
+       totalPages: totalPages,
+     }
+  */
+  const totalResultCount = getResultCount(doc);
+
+  const pagerElement = querySelector('.linkbox.pager', doc)!;
+  const hasChildNodes = pagerElement.hasChildNodes();
+  if (!hasChildNodes) {
+    return {
+      resultsPerPage: totalResultCount,
+      resultsOnThisPage: totalResultCount,
+      totalResultCount: totalResultCount,
+      thisPageRange: { start: totalResultCount === 0 ? 0 : 1, end: totalResultCount },
+      currentPage: 1,
+      totalPages: 1
     };
   }
 
+  const rangeOnThisPage = getPageResultRange(
+    querySelector('.pager.pager_on', pagerElement)!.textContent!
+  );
+  const resultsOnThisPage = resultsInRange(rangeOnThisPage);
+  const rangeOnOtherPage = getPageResultRange(
+    querySelector('.pager.pager_page', pagerElement)!.textContent!
+  );
+  const resultsPerPage = Math.max(resultsOnThisPage, resultsInRange(rangeOnOtherPage));
+
+  // currentPage: can be got a couple of ways, but the simplest seems to be
+  // looking at one of the torrent table's sort button URLs. it's a query
+  // param named `page`, or 1 if it's not there
   const sortableTableHeadingElement = querySelector(
     '.colhead > td:nth-child(5) > a:nth-child(1)',
     doc
@@ -53,14 +152,19 @@ export function getPagination(doc: Document): Pagination {
   const href = sortableTableHeadingElement?.getAttribute('href')!;
   const currentPage = getPageFromUrl(href);
 
+  // totalPages: ditto, but on the link of .pager_last. if there is no .pager_last,
+  // then its the same as currentPage
   const lastPageElement = querySelector('.pager_last', doc);
   const totalPages = lastPageElement
     ? getPageFromUrl(lastPageElement.getAttribute('href')!)
     : currentPage;
 
   return {
+    resultsPerPage,
+    resultsOnThisPage,
+    thisPageRange: rangeOnThisPage,
     currentPage,
     totalPages,
-    resultCount
+    totalResultCount
   };
 }
